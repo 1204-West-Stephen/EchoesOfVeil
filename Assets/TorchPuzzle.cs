ee82729dbb033bd6f66a5980acc32c51ddf1d146
MrBoatCraft <sawest0425@gmail.com>
2025-07-05 16:17:10 -0700

    New scene

---
+++ Assets/TorchPuzzle.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class TorchPuzzle : MonoBehaviour, i_Interactable
+{
+    public ItemData item;
+    public bool itemPickedUp;
+
+    private void Start()
+    {
+        itemPickedUp = false;
+    }
+    public void Interact()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            Inventory inventory = player.GetComponent<Inventory>();
+            if (inventory != null)
+            {
+                if (inventory.CheckInventory())
+                {
+                    inventory.AddItem(item);
+                    Destroy(gameObject);
+                    itemPickedUp = true;
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Player has no Inventory component.");
+            }
+        }
+    }
+    public void DetectPlayer()
+    {
+        
+    }
+    public void ShowUI()
+    {
+
+    }
+    public void HideUI() 
+    { 
+    
+    }
+}
