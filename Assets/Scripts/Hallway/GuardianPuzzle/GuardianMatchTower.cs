0818d1bed798e9d119ca665963acc7f7920c5f32
MrBoatCraft <sawest0425@gmail.com>
2025-09-09 16:05:40 -0700

    Texture updates and the start to the final hallway puzzle

---
+++ Assets/Scripts/Hallway/GuardianPuzzle/GuardianMatchTower.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardianMatchTower : MonoBehaviour, i_Interactable
+{
+    public string towerName;
+
+    public GameObject placedObject;
+    public GameObject answerObject;
+
+    public Transform floatPos;
+
+    private GameObject player;
+    private Inventory inventory;
+
+    private ItemData placedObjectData;
+
+    public bool canPlace;
+    public bool itemPickedUp;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        inventory = player.GetComponent<Inventory>();
+        canPlace = false;
+    }
+
+    public void Interact()
+    {
+        if (canPlace)
+        {
+            if (PlaceObject())
+            {
+                Debug.Log("Object placed");
+                canPlace = false;
+            }
+            else
+            {
+                Debug.Log("Object not placed - no valid object in inventory");
+            }
+        }
+        else
+        {
+            PickUpObject();
+        }
+
+        FindObjectOfType<GuardianPuzzleManager>()?.CheckIfSolved();
+    }
+
+    private bool PlaceObject()
+    {
+        if (inventory == null || inventory.inventory.Count == 0)
+            return false;
+
+        ItemData objectToPlace = null;
+        foreach (var item in inventory.inventory)
+        {
+            if (item != null && item.typeInput == InputType.Tablet)
+            {
+                objectToPlace = item;
+                break;
+            }
+        }
+
+        if (objectToPlace != null)
+        {
+            placedObjectData = objectToPlace;
+
+            inventory.RemoveItem(objectToPlace);
+
+            MoveObjectToShrine(objectToPlace.prefab);
+
+            Debug.Log($"Object {placedObjectData.name} placed in tower {towerName}");
+            return true;
+        }
+
+        return false;
+    }
+
+    private void PickUpObject()
+    {
+        if (placedObject != null && inventory != null && inventory.CheckInventory())
+        {
+            inventory.AddItem(placedObjectData);
+
+            if (placedObject != null)
+            {
+                Destroy(placedObject);
+                placedObject = null;
+            }
+
+            placedObject = null;
+            itemPickedUp = true;
+            canPlace = true;
+
+            Debug.Log("Tablet picked up from tower " + towerName);
+        }
+        else
+        {
+            Debug.LogWarning("Cannot pick up object: inventory full or missing object.");
+        }
+    }
+
+    private void MoveObjectToShrine(GameObject objectPrefab)
+    {
+        if (objectPrefab != null)
+        {
+            if (placedObject != null)
+            {
+                Destroy(placedObject);
+            }
+
+            placedObject = Instantiate(
+                objectPrefab,
+                floatPos.position,
+                Quaternion.Euler(0, 0, 0)
+            );
+
+            placedObject.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("Tablet prefab is null, cannot move to shrine!");
+        }
+    }
+}
---
+++ Assets/Scripts/Hallway/GuardianPuzzle/GuardianPuzzleManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardianPuzzleManager : MonoBehaviour
+{
+    public GuardianMatchTower[] towers;
+    public bool puzzleSolved;
+
+    public void CheckIfSolved()
+    {
+        foreach (GuardianMatchTower tower in towers)
+        {
+            if (tower.placedObject != tower.answerObject)
+            {
+                puzzleSolved = false;
+                return;
+            }
+        }
+
+        puzzleSolved = true;
+        Debug.Log("All towers are correct! Puzzle solved!");
+    }
+}
