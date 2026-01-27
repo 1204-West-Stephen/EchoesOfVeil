08b3786789316bf6b35df6a1a27186957d598348
MrBoatCraft <sawest0425@gmail.com>
2026-01-26 19:23:36 -0800

    stuff

--- Assets/Scripts/SceneLoading/BootStrapper.cs
+++ Assets/Scripts/SceneLoading/BootStrapper.cs
@@ -4,8 +4,7 @@ using UnityEngine.SceneManagement;
 public class Bootstrapper : MonoBehaviour
 {
     void Start()
-    {
-        // Load the first content scene additively
+    { 
         SceneManager.LoadSceneAsync("Jail Cell", LoadSceneMode.Additive);
     }
 }
